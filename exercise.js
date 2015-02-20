f697f7f504e7bd661986cc6b074d9d7afd67f997
stvnksslr <stvnksslr@gmail.com>
2015-02-20 01:35:58 -0500

    feature(querystring): adding query string parsing funtion

--- exercise.js
+++ exercise.js
@@ -1,24 +1,10 @@
 'use strict';
 
-
-function storeValues() {
-    var jsonArray = [{}];
-    var jsonString;
-
-    var formSubmission = document.forms;
-    for (var i = 0; i < formSubmission.length; i++) {
-        if (typeof(jsonArray[0][formSubmission[i].id]) == 'undefined') {
-            jsonArray[0][formSubmission[i].id] = [];
-        }
-        for (var ii = 0; ii < formSubmission[i].elements.length; ii++) {
-            jsonArray[0][formSubmission[i].id].push(formSubmission[i].elements[ii].value);
-        }
-    }
-
-    jsonString = JSON.stringify(jsonArray);
-    console.log(jsonString);
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
-window.onload = function() {
-    document.getElementById('submitJSON').onclick = storeValues;
-};
+var prodId = getParameterByName('prodId');
+
+console.log('testing: ' + prodId);
