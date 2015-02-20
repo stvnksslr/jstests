30a3252e654a869ed713e3e2329c1b8c2d0e3a33
stvnksslr <stvnksslr@gmail.com>
2015-02-20 10:29:40 -0500

    initial commit

--- jstest.js
+++ jstest.js
@@ -1,48 +1,21 @@
-'use strict';
+ angular.module('formExample', [])
+     .controller('ExampleController', ['$scope', function($scope) {
+         $scope.master = {};
 
-// wrapping the parser and its variables in its own function
-function queryStringParsing() {
-    // slicing and seperating query string by & symbol	
-    var queryString = location.search.slice(1).split('&');
-    var result = {};
-    queryString.forEach(function(processedString) {
-        processedString = processedString.split('=');
-        result[processedString[0]] = decodeURIComponent(processedString[1] || '');
-    });
-    // for the final goal of converting to local storage, I am turning the above seperated 
-    // slices and converting them to the json format
-    function convertToJSON() {
-        var queryString = location.search.slice(1).split('&');
-        var result = {};
-        queryString.forEach(function(processedString) {
-            processedString = processedString.split('=');
-            result[processedString[0]] = decodeURIComponent(processedString[1] || '');
-        });
-        return JSON.parse(JSON.stringify(result));
-    }
+         $scope.update = function(user) {
+             $scope.master = angular.copy(user);
+         };
 
-    function SetCookie(cookieName, cookieStr, Days) {
-        var today = new Date();
-        var expire = new Date();
-        Days = 3;
-        var cookieName = 'jsTest';
-        var cookieStr = JSON.stringify(result);
-        expire.setTime(today.getTime() + 3600000 * 24 * Days);
-        document.cookie = cookieName + '=' + (cookieStr) + ';expires=' + expire.toGMTString();
-    }
+         $scope.reset = function() {
+             $scope.user = angular.copy($scope.master);
+         };
 
-    // adding string to local storage 
-    localStorage['jsTestStorage'] = JSON.stringify(result);
-
-    // outputting the data for testing/validation
-    var finalizedString = convertToJSON();
-    var initiateCookie = SetCookie();
-    console.log(finalizedString);
-}
-
-// starting the parser
-var initiateParser = queryStringParsing();
+         $scope.reset();
+     }]);
 
 
-// Article used for query string processing: http://www.developerdrive.com/2013/08/turning-the-querystring-into-a-json-object-using-javascript/
-// Article used for cookie processing http://www.w3schools.com/js/js_cookies.asp
+ it('should toggle readonly attr', function() {
+     expect(element(by.css('[type="text"]')).getAttribute('readonly')).toBeFalsy();
+     element(by.model('checked')).click();
+     expect(element(by.css('[type="text"]')).getAttribute('readonly')).toBeTruthy();
+ });
