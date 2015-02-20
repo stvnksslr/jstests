ac21a72af93cb0fbef0152e04881092ed62868a5
stvnksslr <stvnksslr@gmail.com>
2015-02-20 00:07:54 -0500

    feature(json): json submission to log finished

--- exercise.js
+++ exercise.js
@@ -1,15 +1,24 @@
 'use strict';
 
-serialize = function(obj) {
-    var str = [];
-    for (var p in obj)
-        if (obj.hasOwnProperty(p)) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+
+function storeValues() {
+    var arr = [{}];
+    var jsonString;
+
+    var forms = document.forms;
+    for (var i = 0; i < forms.length; i++) {
+        if (typeof(arr[0][forms[i].id]) == 'undefined') {
+            arr[0][forms[i].id] = [];
         }
-    return str.join("&");
+        for (var ii = 0; ii < forms[i].elements.length; ii++) {
+            arr[0][forms[i].id].push(forms[i].elements[ii].value);
+        }
+    }
+
+    jsonString = JSON.stringify(arr);
+    console.log(jsonString);
 }
 
-console.log(serialize({
-    foo: "hi there",
-    bar: "100%"
-}));
+window.onload = function() {
+    document.getElementById("submitJSON").onclick = storeValues;
+};
