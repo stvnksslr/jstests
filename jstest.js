f2d64919ab55797ad06174be0fb1a2c97228a6e2
stvnksslr <stvnksslr@gmail.com>
2015-02-21 14:12:02 -0500

    commit

--- jstest.js
+++ jstest.js
@@ -2,6 +2,9 @@
 
 angular.module('formApp', [])
     .controller('MyFormController', ['$scope', function($scope) {
+        // wasnt sure the proper practice for this actually but this will push these values in
+        // to form data, one fix i should probably make for this is having it load from defaultData on 
+        // page load and then not to reference it. 
         $scope.formData = {
             'firstname': 'steve',
             'lastname': 'kessler',
@@ -9,6 +12,8 @@ angular.module('formApp', [])
             'email': 'stvnksslr@gmail.com'
         };
 
+        // Default data object used for replacing the fields back to there editible state when
+        // toggled
         $scope.defaultData = {
             'firstname': 'steve',
             'lastname': 'kessler',
@@ -16,21 +21,29 @@ angular.module('formApp', [])
             'email': 'stvnksslr@gmail.com'
         };
 
+        // Control for whether a field will be editable
         $scope.formOptions = {
             disableForm: true
         };
 
+        // loading the submit button as false so that it is not present at page load
         $scope.showSubmit = false;
 
     }])
     .controller('FormToggleController', ['$scope', function($scope) {
 
         $scope.toggleDisableForm = function() {
+            // evaluating the boolean value for the disableForm to always be the opposte of 
+            // its current status
             $scope.formOptions.disableForm = !($scope.formOptions.disableForm);
+            //logic for enabling the fields when the fields are not editable and 
+            //injecting the default data.
             if ($scope.formOptions.disableForm === true) {
                 console.log('bing bool loop');
                 $scope.formData = $scope.defaultData;
             } else {
+                // eval to always show submit button when the fields are in an 
+                // editable state.
                 $scope.showSubmit = true;
             }
 
@@ -41,6 +54,7 @@ angular.module('formApp', [])
             restrict: 'E',
             scope: "=",
             controller: 'FormToggleController',
+            // toggle directive html
             template: '<button ng-click="toggleDisableForm()"> Click here to lock/unlock fields </button>'
         };
     }]);
