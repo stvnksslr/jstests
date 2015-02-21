0d2d0e22f5e05b343bb177162f4bc16b1d41b582
stvnksslr <stvnksslr@gmail.com>
2015-02-21 14:06:37 -0500

    commit

--- jstest.js
+++ jstest.js
@@ -1,14 +1,46 @@
 'use strict';
 
-var formApp = angular.module('myModule', []).controller('MyFormCtrl', ['$scope', function($scope) {
-    $scope.formData = {
-        'firstname': 'steve',
-        'lastname': 'kessler',
-        'phonenumber': '123-4567',
-        'email': 'stvnksslr@gmaial.com'
-    };
-    $scope.formOptions = {
-        disableForm: true
-    };
-}]);
+angular.module('formApp', [])
+    .controller('MyFormController', ['$scope', function($scope) {
+        $scope.formData = {
+            'firstname': 'steve',
+            'lastname': 'kessler',
+            'phonenumber': '123-4567',
+            'email': 'stvnksslr@gmail.com'
+        };
 
+        $scope.defaultData = {
+            'firstname': 'steve',
+            'lastname': 'kessler',
+            'phonenumber': '123-4567',
+            'email': 'stvnksslr@gmail.com'
+        };
+
+        $scope.formOptions = {
+            disableForm: true
+        };
+
+        $scope.showSubmit = false;
+
+    }])
+    .controller('FormToggleController', ['$scope', function($scope) {
+
+        $scope.toggleDisableForm = function() {
+            $scope.formOptions.disableForm = !($scope.formOptions.disableForm);
+            if ($scope.formOptions.disableForm === true) {
+                console.log('bing bool loop');
+                $scope.formData = $scope.defaultData;
+            } else {
+                $scope.showSubmit = true;
+            }
+
+        };
+    }])
+    .directive('formToggle', [function() {
+        return {
+            restrict: 'E',
+            scope: "=",
+            controller: 'FormToggleController',
+            template: '<button ng-click="toggleDisableForm()"> Click here to lock/unlock fields </button>'
+        };
+    }]);
